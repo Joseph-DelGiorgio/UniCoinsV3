814a9a06ada1c9ff37cae566bbe2f62fd94b234e
Joseph Del Giorgio <106563909+Joseph-DelGiorgio@users.noreply.github.com>
2023-04-10 18:42:47 -0700

    Update hardhat.config.js

--- my-app/hardhat.config.js
+++ my-app/hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('@nomiclabs/hardhat-ethers');
+require("@nomicfoundation/hardhat-chai-matchers");
+require('dotenv').config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const { GOERLI_PRIVATE_KEY, INFURA_PROJECT_ID } = process.env;
+
 module.exports = {
-  solidity: "0.8.17",
+  solidity: '0.8.0',
+  networks: {
+    goerli: {
+      url: `https://goerli.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [GOERLI_PRIVATE_KEY],
+    },
+  },
 };
