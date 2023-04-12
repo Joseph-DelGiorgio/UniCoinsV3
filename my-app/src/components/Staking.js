e27230effb95352b27867eadfa6827216bbcfe61
Joseph Del Giorgio <106563909+Joseph-DelGiorgio@users.noreply.github.com>
2023-04-11 19:26:45 -0700

    Update Staking.js

--- my-app/src/components/Staking.js
+++ my-app/src/components/Staking.js
@@ -1,26 +1,58 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { Web3Context } from "/Users/josephdelgiorgio/UniCoinsV3/my-app/src/contexts/Web3Context.js";
 import "/Users/josephdelgiorgio/UniCoinsV3/my-app/src/components/Staking.css";
 import Dashboard from "./Dashboard";
-import ProjectContext from "./ProjectContext";
+import ProjectContext from "/Users/josephdelgiorgio/UniCoinsV3/my-app/src/components/ProjectContext.js";
+import UNCollaborationABI from '/Users/josephdelgiorgio/UniCoinsV3/my-app/src/abis/UNCollaboration.json';
 
-const Staking = ({ stakeTokens, unstakeTokens, balance, stakingPosition }) => {
+const Staking = ({ balance, stakingPosition }) => {
+  const { UNCollaborationContract, account } = useContext(Web3Context);
+
   const [stakeAmount, setStakeAmount] = useState(0);
   const [unstakeAmount, setUnstakeAmount] = useState(0);
 
-  const handleStake = () => {
-    stakeTokens(stakeAmount);
+  const handleStake = async () => {
+    try {
+      console.log("Starting handleStake...");
+      if (UNCollaborationContract && account) {
+        console.log("UNCollaborationContract and account are valid...");
+        const weiAmount = window.web3.utils.toWei(stakeAmount, "ether");
+        console.log("weiAmount:", weiAmount);
+        await UNCollaborationContract.methods
+          .stakeTokens(weiAmount)
+          .send({ from: account });
+        console.log("Staking successful");
+      }
+    } catch (error) {
+      console.error("Error while staking:", error);
+    }
+  };
+  
+
+  const handleUnstake = async () => {
+    try {
+      if (UNCollaborationContract && account) {
+        await UNCollaborationContract.methods
+          .unstakeTokens(unstakeAmount)
+          .send({ from: account });
+      }
+    } catch (error) {
+      console.error("Error while unstaking:", error);
+    }
   };
 
-  const handleUnstake = () => {
-    unstakeTokens(unstakeAmount);
-  };
-
-  // Replace with your initial projects and tasks data
   const initialProjects = [];
   const initialTasks = [];
 
+  const addProject = (project) => {
+    setProjects((prevProjects) => [...prevProjects, project]);
+  };
+
+  const [projects, setProjects] = useState(initialProjects);
+  const [tasks, setTasks] = useState(initialTasks);
+
   return (
-    <ProjectContext.Provider value={{ projects: initialProjects, tasks: initialTasks }}>
+    <ProjectContext.Provider value={{ projects, tasks, addProject }}>
       <div>
         <h2>Staking</h2>
         <div>
@@ -30,7 +62,9 @@ const Staking = ({ stakeTokens, unstakeTokens, balance, stakingPosition }) => {
             value={stakeAmount}
             onChange={(e) => setStakeAmount(e.target.value)}
           />
-          <button className="button" onClick={handleStake}>Stake</button>
+          <button className="button" onClick={handleStake}>
+            Stake
+          </button>
         </div>
         <div>
           <input
@@ -39,7 +73,9 @@ const Staking = ({ stakeTokens, unstakeTokens, balance, stakingPosition }) => {
             value={unstakeAmount}
             onChange={(e) => setUnstakeAmount(e.target.value)}
           />
-          <button className="button" onClick={handleUnstake}>Unstake</button>
+          <button className="button" onClick={handleUnstake}>
+            Unstake
+          </button>
         </div>
         <p>Your Staking Balance: {balance} Tokens</p>
         <p>Your Staking Position: {stakingPosition} Tokens</p>
