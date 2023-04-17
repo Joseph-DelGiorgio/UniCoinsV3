de4ff6e8f81471b5286feffbef83245e0835aa9e
Joseph Del Giorgio <106563909+Joseph-DelGiorgio@users.noreply.github.com>
2023-04-16 18:49:06 -0700

    Update Dashboard.js

--- my-app/src/components/Dashboard.js
+++ my-app/src/components/Dashboard.js
@@ -5,58 +5,85 @@ import Pagination from "./Pagination";
 import SearchBar from "./SearchBar";
 import ProjectCharts from "./ProjectCharts";
 import ProjectContext from "./ProjectContext";
+import { Web3Context } from "../contexts/Web3Context"; // Corrected import statement
 
 const Dashboard = () => {
-  const { projects: initialProjects } = useContext(ProjectContext);
-  const [projects, setProjects] = useState([]);
+  const { projects } = useContext(ProjectContext);
+  const { volunteerKPIs, loading } = useContext(Web3Context);
   const [filteredProjects, setFilteredProjects] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [projectsPerPage] = useState(10);
 
   useEffect(() => {
-    setProjects(initialProjects);
-    setFilteredProjects(initialProjects);
-  }, [initialProjects]);
+    setFilteredProjects(projects);
+  }, [projects]);
 
-  const filterAndSortProjects = (filters, sortOptions) => {
-    // Apply filters and sort options to the projects and set the filteredProjects state
+  const filterAndSortProjects = (category, sortOrder) => {
+    const filtered = projects.filter(project =>
+      category ? project.category === category : true
+    );
+    const sorted = filtered.sort((a, b) => {
+      if (sortOrder === "asc") {
+        return a.title.localeCompare(b.title);
+      } else {
+        return b.title.localeCompare(a.title);
+      }
+    });
+    setFilteredProjects(sorted);
   };
 
   const searchProjects = (searchTerm) => {
-    // Search projects by title or description and set the filteredProjects state
+    const searchResults = projects.filter((project) =>
+      project.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setFilteredProjects(searchResults);
   };
 
   const indexOfLastProject = currentPage * projectsPerPage;
   const indexOfFirstProject = indexOfLastProject - projectsPerPage;
-  const currentProjects = filteredProjects.slice(
-    indexOfFirstProject,
-    indexOfLastProject
-  );
+  const currentProjects = filteredProjects.slice(indexOfFirstProject, indexOfLastProject);
 
-  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+  const paginate = pageNumber => setCurrentPage(pageNumber);
 
   return (
     <div className="dashboard">
-      <h2>Dashboard</h2>
-      <SearchBar searchProjects={searchProjects} />
-      <FilterOptions filterAndSortProjects={filterAndSortProjects} />
-      {/* Use the filteredProjects for the ProjectCharts */}
-      <ProjectCharts projects={filteredProjects} />
-      <div className="project-list">
-        {currentProjects.map((project) => (
-          <div key={project.id} className="project">
-          {/* Render project details here */}
+      {!loading ? (
+        <div className="volunteer-kpis">
+          <h2>Volunteer KPIs</h2>
+          <p>Completed Tasks: {volunteerKPIs.completedTasks}</p>
+          {volunteerKPIs.onTimeCompletionRate !== undefined && (
+          <p>
+            On-Time Completion Rate:{" "}
+            {volunteerKPIs.onTimeCompletionRate.toFixed(2)}
+          </p>
+          )}
+           {volunteerKPIs.managerRatings !== undefined && (
+          <p>
+            Average Manager Ratings:{" "}
+            {volunteerKPIs.managerRatings.toFixed(2)}
+          </p>
+           )}
+          <p>Other Metrics: {volunteerKPIs.otherMetrics}</p>
         </div>
-      ))}
+      ) : (
+        <div className="volunteer-kpis">
+          <h2>Volunteer KPIs</h2>
+          <p>Loading...</p>
+        </div>
+      )}
+      <div className="filter-section">
+        <FilterOptions filterAndSortProjects={filterAndSortProjects} />
+        <SearchBar searchProjects={searchProjects} />
+      </div>
+      <ProjectCharts projects={currentProjects} />
+      <Pagination
+        projectsPerPage={projectsPerPage}
+        totalProjects={filteredProjects.length}
+        paginate={paginate}
+      />
     </div>
-    <Pagination
-      projectsPerPage={projectsPerPage}
-      totalProjects={filteredProjects.length}
-      paginate={paginate}
-    />
-  </div>
-);
+  );
 };
+
 export default Dashboard;
-         
 
