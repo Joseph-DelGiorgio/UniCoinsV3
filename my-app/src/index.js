189a1e83980313d5a304471419025b94a8509fa6
Joseph Del Giorgio <106563909+Joseph-DelGiorgio@users.noreply.github.com>
2023-04-23 13:18:49 -0700

    Update index.js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,71 +1,15 @@
-/*
-const AppWrapper = () => {
-  const [account, setAccount] = useState(null);
-  const [UNCollaborationContract, setUNCollaborationContract] = useState(null);
-
-  useEffect(() => {
-    const fetchAccount = async () => {
-      const accounts = await web3.eth.getAccounts();
-      if (accounts && accounts.length > 0) {
-        setAccount(accounts[0]);
-      }
-    };
-
-    fetchAccount();
-  }, []);
-
-  useEffect(() => {
-    const loadContract = async () => {
-      if (web3 && account) {
-        const networkId = await web3.eth.net.getId();
-        const contractData = UNCollaboration.networks[networkId];
-
-        if (contractData) {
-          const contractInstance = new web3.eth.Contract(UNCollaboration.abi, contractData.address);
-          setUNCollaborationContract(contractInstance);
-        } else {
-          console.error("UNCollaborationContract not deployed to the detected network");
-        }
-      }
-    };
-
-    loadContract();
-  }, [web3, account]);
-  return (
-    <App/>
-  );
-};
-
-createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Router>
-      <AppWrapper />
-    </Router>
-  </React.StrictMode>
-);
-*/
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './components/App';
-import Home from './components/Home';
-import Tasks from './components/Tasks';
-import Task from './components/Task';
-import Projects from './components/Projects';
-import Badges from './components/Badges';
-import AppNavbar from './components/AppNavbar';
-import Staking from './components/Staking';
-import Web3 from 'web3';
-import ProposeProject from './components/ProposeProject'; // Add this line
-import UNCollaboration from './abis/UNCollaboration.json';
-
-
-const web3 = new Web3(Web3.givenProvider || 'http://localhost:3000');
+import { Web3Provider } from './contexts/Web3Context'; // Add this import
 
 const AppWrapper = () => {
   return (
-    <App/>
+    <Web3Provider> {/* Add the Web3Provider wrapper here */}
+      <App/>
+    </Web3Provider>
   );
 };
 
