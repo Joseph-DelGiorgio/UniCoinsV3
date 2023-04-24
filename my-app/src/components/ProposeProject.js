52f243b459202c9765a4e9ecb108758a913b1adf
Joseph Del Giorgio <106563909+Joseph-DelGiorgio@users.noreply.github.com>
2023-04-24 16:21:27 -0700

    Update ProposeProject.js

--- my-app/src/components/ProposeProject.js
+++ my-app/src/components/ProposeProject.js
@@ -3,7 +3,6 @@ import './ProposeProject.css';
 import ProjectContext from './ProjectContext';
 import { Web3Context } from '../contexts/Web3Context';
 
-
 const ProposeProject = () => {
   const { web3, contract, account } = useContext(Web3Context);
   const { addProject } = useContext(ProjectContext);
@@ -13,23 +12,28 @@ const ProposeProject = () => {
 
   const onSubmit = async (event) => {
     event.preventDefault();
-  
+
     if (!web3) {
       alert('Web3 is not initialized. Please check your MetaMask connection.');
       return;
     }
-  
+
     if (!contract) {
       alert('Contract is not initialized. Please check your configuration.');
       return;
     }
-  
+
+    if (!account) {
+      alert('Account is not available. Please check your MetaMask connection.');
+      return;
+    }
+
     const newProject = {
       title,
       description,
       budget: parseFloat(budget).toFixed(2),
     };
-  
+
     const budgetWei = web3.utils.toWei(budget, 'ether');
     try {
       await contract.methods.proposeProject(description, budgetWei).send({ from: account });
@@ -37,14 +41,13 @@ const ProposeProject = () => {
       console.error('Error sending transaction:', error);
       return;
     }
-  
+
     addProject(newProject);
-  
+
     setTitle('');
     setDescription('');
     setBudget('');
   };
-  
 
   return (
     <div className="container">
@@ -56,27 +59,27 @@ const ProposeProject = () => {
           id="title"
           value={title}
           onChange={(e) => setTitle(e.target.value)}
-          required
-        />
-        <label htmlFor="description">Description:</label>
-        <textarea
-          id="description"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-          required
-        ></textarea>
-        <label htmlFor="budget">Budget (ETH):</label>
-        <input
-          type="number"
-          id="budget"
-          value={budget}
-          onChange={(e) => setBudget(e.target.value)}
-          required
-        />
-        <button type="submit">Submit</button>
-      </form>
-    </div>
-  );
+      required
+    />
+    <label htmlFor="description">Description:</label>
+    <textarea
+      id="description"
+      value={description}
+      onChange={(e) => setDescription(e.target.value)}
+      required
+    ></textarea>
+    <label htmlFor="budget">Budget (ETH):</label>
+    <input
+      type="number"
+      id="budget"
+      value={budget}
+      onChange={(e) => setBudget(e.target.value)}
+      required
+    />
+    <button type="submit">Submit</button>
+  </form>
+</div>
+);
 };
 
 export default ProposeProject;
