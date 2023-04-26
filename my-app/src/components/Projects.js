6261ca57b5222a5b3f08f6ec8d52013b5136c86f
Joseph Del Giorgio <106563909+Joseph-DelGiorgio@users.noreply.github.com>
2023-04-25 17:49:52 -0700

    Update Projects.js

--- my-app/src/components/Projects.js
+++ my-app/src/components/Projects.js
@@ -4,6 +4,9 @@ import ProposeProject from "./ProposeProject";
 import "./Projects.css";
 import ProjectContext from "./ProjectContext";
 import { useWeb3 } from '/Users/josephdelgiorgio/UniCoinsV3/my-app/src/contexts/Web3Context.js';
+import leftImage from '/Users/josephdelgiorgio/UniCoinsV3/my-app/src/assets/UNicoinGold.webp';
+import rightImage from '/Users/josephdelgiorgio/UniCoinsV3/my-app/src/assets/UNicoinGold.webp';
+
 
 const Projects = () => {
   const { web3, contract, account } = useWeb3();
@@ -16,13 +19,19 @@ const Projects = () => {
   return (
     <ProjectContext.Provider value={{ projects, addProject }}>
       <div className="container">
-        <h1>Projects</h1>
-        <ProjectList projects={projects} web3={web3} />
-        <ProposeProject web3={web3} addProject={addProject} />
+        <img className="left-image" src={leftImage} alt="Left side" />
+        <img className="right-image" src={rightImage} alt="Right side" />
+        <div className="main-content">
+          <h1>Projects</h1>
+          <ProjectList projects={projects} web3={web3} />
+          <ProposeProject web3={web3} addProject={addProject} />
+        </div>
       </div>
     </ProjectContext.Provider>
   );
+  
+  
+  
 };
 
 export default Projects;
-
